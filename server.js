7a932f7b4211789f1739b1b45ef4459854c963ad
AniruddhaKanere <aniruddhakanere7@gmail.com>
2024-07-19 17:12:29 +0530

    Added route to fetch articles in Express

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Sample articles data
+const articles = [
+  { id: 1, title: 'Understanding Small Cap Companies' },
+  { id: 2, title: 'Investment Strategies for Small Cap Stocks' }
+];
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Route to get the welcome message
 app.get('/', (req, res) => {
     res.send('Welcome to the Small Cap Companies Blog!');
+});
+
+// Route to get articles
+app.get('/api/articles', (req, res) => {
+    res.json(articles);
+});
+
+// Catch all other routes and return the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
